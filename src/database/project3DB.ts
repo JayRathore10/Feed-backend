95fa6bf620c681d45496f1a6df0a9e9b39fd5266
Jay <jayrathore88155@gmail.com>
2025-11-21 22:52:27 +0530

    Add mongoDB Atlas

--- src/database/project3DB.ts
+++ src/database/project3DB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = ()=>{
   try{
-    mongoose.connect(`mongodb://localhost:27017/project3`);
+    mongoose.connect(`mongodb+srv://JayRathore10:Jay9575Rathore@blogdb.kkimp1o.mongodb.net/`);
     console.log("Database connected");
   }catch(err){
     console.log(err);
