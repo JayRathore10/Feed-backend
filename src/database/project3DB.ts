47ed74e68054d1afb03b7a78303a6b52839f25f8
Jay <jayrathore88155@gmail.com>
2025-11-21 23:13:06 +0530

    DB fix

--- src/database/project3DB.ts
+++ src/database/project3DB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = ()=>{
   try{
-    mongoose.connect(`mongodb+srv://JayRathore10:Jay9575Rathore@blogdb.kkimp1o.mongodb.net/`);
+    mongoose.connect(`mongodb+srv://JayRathore10:Jay9575Rathore@blogdb.kkimp1o.mongodb.net/blogDB`);
     console.log("Database connected");
   }catch(err){
     console.log(err);
